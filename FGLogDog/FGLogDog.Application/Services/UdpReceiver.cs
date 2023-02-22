84ba484d4fcc616abfb185ecd65b4900c5b8ef60
Strauberg <stefanstrauberg@gmail.com>
2023-02-22 16:49:39 +0300

    udp srv

--- FGLogDog/FGLogDog.Application/Services/UdpReceiver.cs
+++ FGLogDog/FGLogDog.Application/Services/UdpReceiver.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using FGLogDog.Application.Helper;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FGLogDog.Application.Services
 {
@@ -10,17 +14,29 @@ namespace FGLogDog.Application.Services
         private readonly string _input;
         private readonly string _srcip;
         private readonly string _srcport;
+        private readonly ILogger<UdpReceiver> _logger;
 
-        public UdpReceiver(IConfiguration configuration)
+        public UdpReceiver(IConfiguration configuration,
+                           ILogger<UdpReceiver> logger)
         {
             _input = configuration.GetSection("ConfigurationString").GetSection("Input").Value;
             ParserFactory.SearchSubstring(_input, "srcip=", ParserTypes.IP, out _srcip);
             ParserFactory.SearchSubstring(_input, "srcport=", ParserTypes.INT, out _srcport);
+            _logger = logger;
         }
 
-        public Task Start()
+        public async Task Start()
         {
-            throw new System.NotImplementedException();
+            using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var localIP = new IPEndPoint(IPAddress.Parse(_srcip), Int32.Parse(_srcport));
+            udpSocket.Bind(localIP);
+
+            _logger.LogInformation($"LogDog server ready to recive messages on ip:{_srcip} and port:{_srcport}");
+
+            byte[] data = new byte[256];
+            EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+            var result = await udpSocket.ReceiveFromAsync(data, remoteIp);
+            var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
         }
 
         public void Dispose()
