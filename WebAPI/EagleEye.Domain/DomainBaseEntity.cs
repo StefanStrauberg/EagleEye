c2d446fd2d47041b9ef9ad2ae645e1edc4c2a735
Strauberg <stefanstrauberg@gmail.com>
2023-02-28 17:15:30 +0300

    fix 16

---
+++ WebAPI/EagleEye.Domain/DomainBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EagleEye.Domain
+{
+    public class DomainBaseEntity
+    {
+        public Guid Id { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
--- WebAPI/EagleEye.Infrastructure/Repositories/CollectionRepository.cs
+++ WebAPI/EagleEye.Infrastructure/Repositories/CollectionRepository.cs
@@ -1,4 +1,6 @@
-﻿using EagleEye.Infrastructure.DatabaseConfig;
+﻿using EagleEye.Domain;
+using EagleEye.Infrastructure.DatabaseConfig;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,10 +10,10 @@ namespace EagleEye.Infrastructure.Repositories
 {
     internal class CollectionRepository : ICollectionRepository
     {
-        private readonly IMongoDBConnection _connection;
+        private readonly IMongoDatabase _database;
 
         public CollectionRepository(IMongoDBConnection connection)
-            => _connection = connection;
+            => _database = new MongoClient(connection.ConnectionString).GetDatabase(connection.DatabaseName);
 
         public Task<bool> CreateAsync(string collectionName, object model)
         {
@@ -23,15 +25,15 @@ namespace EagleEye.Infrastructure.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<IReadOnlyList<object>> GetAllAsync(string collectionName)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IReadOnlyList<object>> GetAllAsync(string collectionName)
+            => await _database.GetCollection<DomainBaseEntity>(collectionName)
+                              .Find(x => true)
+                              .ToListAsync();
 
-        public Task<object> GetByIdAsync(string collectionName, Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<object> GetByIdAsync(string collectionName, Guid id)
+            => await _database.GetCollection<DomainBaseEntity>(collectionName)
+                              .Find(x => x.Id == id)
+                              .FirstOrDefaultAsync();
 
         public Task<bool> UpdateAsync(string collectionName, object model)
         {
